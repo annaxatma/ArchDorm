35bf3ac96c62a90b26134238ff166fa985c077df
annaxatma <83283641+annaxatma@users.noreply.github.com>
2022-05-28 16:39:48 +0700

    start

---
+++ ArchDorm/ArchDormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchDormApp.swift
+//  ArchDorm
+//
+//  Created by MacBook Pro on 28/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ArchDormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
